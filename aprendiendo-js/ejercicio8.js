66018e5f4c35f9fc501948b052fe8a44b6328353
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-01 10:58:16 -0500

    40

--- aprendiendo-js/ejercicio8.js
+++ aprendiendo-js/ejercicio8.js
@@ -9,13 +9,16 @@ en el body de la pagina, en una alerta y por la consola el resultado de sumar, r
 let num1 = parseInt(prompt("Introduce el primer numero", 0));
 let num2 = parseInt(prompt("Introduce el segundo numero", 0));
 
-while(num1 < 0 || num2 < 0 || isNAN(num1) || isNAN(num2)){
+while(num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2)){
     num1 = parseInt(prompt("Introduce el primer numero", 0));
     num2 = parseInt(prompt("Introduce el segundo numero", 0));
 }   
 
 let result = "la Suma es: " +(num1 + num2)+"<br>" +
             "la Resta es: " +(num1 - num2)+"<br>"+
-            "la Multiplicacion es: " +(num1 * num2)+"<br>" 
-            "la Division es: " +(num1 / num2)+"<br>"
+            "la Multiplicacion es: " +(num1 * num2)+"<br>"+ 
+            "la Division es: " +(num1 / num2)+"<br>";
+
+document.write(result);
+
 
