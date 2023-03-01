3357a28692569fce163e446f7c11e3d08c301f0e
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-01 10:52:51 -0500

    40. Ejericio 8

---
+++ aprendiendo-js/ejercicio8.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+Una calculador
+que pidas 2 numero por patalla
+que metemos uno mal que nos lo vuelva a pedir
+en el body de la pagina, en una alerta y por la consola el resultado de sumar, restar, multiplacar y dividir esas dos cifras
+*/
+let num1 = parseInt(prompt("Introduce el primer numero", 0));
+let num2 = parseInt(prompt("Introduce el segundo numero", 0));
+
+while(num1 < 0)|| num2 < 0 || isNAN(num1) || isNAN(num2){
+    num1 = parseInt(prompt("Introduce el primer numero", 0));
+    num2 = parseInt(prompt("Introduce el segundo numero", 0));
+}   
+
+let result = "la Suma es: " +(num1 + num2)+"<br>" +
+            "la Resta es: " +(num1 - num2)+"<br>"+
+            "la Multiplicacion es: " +(num1 * num2)+"<br>" 
+            "la Division es: " +(num1 / num2)+"<br>"
+
