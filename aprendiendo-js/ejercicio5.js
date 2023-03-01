a782364841b4adc33543871c76534ab480980a9b
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-01 10:28:26 -0500

    37. Ejercicio5

---
+++ aprendiendo-js/ejercicio5.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*Muestre todos los numero divisores de un numero introducido por el usuario*/
+
+let num1 = parseInt(prompt("Introduce el primer numero", 1));
+
+for(let i = 1; i <= num1; i++){
+    if(num1%i == 0){
+        console.log("Divisor: "+i)
+    }
+    
+}
